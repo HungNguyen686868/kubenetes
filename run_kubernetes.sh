54a652f430d1a7fe87304498d6e4352fb7bee004
Hung Nguyen <nguyenphunghung1511@gmail.com>
2022-10-20 14:49:00 +0700

    move from ec2 to local

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl apply -f test_deployment_app.yaml
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl apply -f service_deployment.yaml
